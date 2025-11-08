4f8531ab5ebcdbcceb01210647ab92bb4a2ac481
arun <allamarunkumar@gmail.com>
2025-11-08 18:47:05 +0530

    Multithreading

---
+++ Multithreadig/Class17.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet6pmto7pm
+{
+    class Demo
+    {
+        public void Test()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} . Welcome to DotNet Training");
+            }
+        }
+        public void Hi()
+        {
+            for(int i = 1;i <= 10; i++)
+            {
+                Console.WriteLine($"{i} Hi this is hi mehod from class Demo");
+            }
+        }
+        internal class Class17
+        {
+            static void Main(string[] args)
+            {
+                Demo p = new Demo();
+                p.Test();
+                p.Hi();
+
+            }
+        }
+    }
+}
---
+++ Multithreadig/Class18.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace DotNet6pmto7pm
+{ 
+
+    class Game
+    {
+        string name;
+       public Game(string name) { 
+        this.name =name;
+        }
+        public void Player()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{name} is Playing {i} Time");
+                Thread.Sleep(1000);
+            }
+        }
+        
+    }
+    internal class Class18
+    {
+        static void Main(string[] args)
+        {
+            Game p1 = new Game("kiran");
+            Thread th1 = new Thread(p1.Player);  // un Started state
+           
+            Game p2 = new Game("Raj");
+            Thread th2 = new Thread(p2.Player);
+            th1.Name = "Kiran";
+            th2.Name = "Raj"; // settig the name
+
+            th1.Priority = ThreadPriority.Lowest;
+            th2.Priority = ThreadPriority.Highest;
+
+
+            Console.WriteLine($"First Thread Name : {th1.Name}");
+            Console.WriteLine($"Second Thread Name : {th2.Name}");  //Getting name
+
+            th1.Start();     // ready state
+            th1.Join(5000);  // non runable state.
+
+            th2.Start(); //Ready state
+           
+
+        }
+    }
+}
